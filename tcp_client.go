03e397e622d5d644c2a2c8447b46c06983a5a86e
“Computarfant” <lennartbirk@gmail.com>
2023-04-21 01:28:42 +0200

    asdas

---
+++ tcp_client.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"log"
+	"net"
+	"os"
+
+	"github.com/Computarfant/is105sem03/mycrypt"
+)
+
+func main() {
+	conn, err := net.Dial("tcp", "172.17.0.4:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("os.Args[1] = ", os.Args[1])
+
+	// Encrypt the message
+	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
+
+	_, err = conn.Write([]byte(string(kryptertMelding)))
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf := make([]byte, 1024)
+	n, err := conn.Read(buf)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// Decrypt the response from the server
+	deCrypt := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Println("Dekrypter melding: ", string(deCrypt))
+
+	response := string(deCrypt)
+	log.Printf("reply from proxy: %s", response)
+
+}
+//hei
