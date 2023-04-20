b6f58e03e553d95f0cd5488dad7e743a98045f18
“Computarfant” <lennartbirk@gmail.com>
2023-04-20 20:15:32 +0200

    decrypt

--- tcp_client.go
+++ tcp_client.go
@@ -9,7 +9,7 @@ import (
 )
 
 func main() {
-	conn, err := net.Dial("tcp", "172.17.0.2:8000")
+	conn, err := net.Dial("tcp", "172.17.0.4:8000")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -25,10 +25,19 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	response := string(buf[:n])
+
+	// Decrypt the response from the server
+	dekryptertMelding := mycrypt.Krypter([]rune(string(buf[:n])), mycrypt.ALF_SEM03, len(mycrypt.ALF_SEM03)-4)
+	log.Println("Dekrypter melding: ", string(dekryptertMelding))
+
+	response := string(dekryptertMelding)
 	log.Printf("reply from proxy: %s", response)
 
+	// Encrypt the message and send it back to the server
 	kryptertMelding := mycrypt.Krypter([]rune(os.Args[1]), mycrypt.ALF_SEM03, 4)
 	log.Println("Kryptert melding: ", string(kryptertMelding))
 	_, err = conn.Write([]byte(string(kryptertMelding)))
+	if err != nil {
+		log.Fatal(err)
+	}
 }
